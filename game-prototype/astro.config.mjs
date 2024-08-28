f3994f2a9d8a4200c6587fbcf25c3d8b820b777b
Danilo Nobre <danilonobren@gmail.com>
2024-08-28 15:55:34 -0300

    reeee

--- game-prototype/astro.config.mjs
+++ game-prototype/astro.config.mjs
@@ -1,25 +1,17 @@
-import { resolve } from 'path';
-import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = resolve(__filename, '..');
 
 export default defineConfig({
   integrations: [
     solid(),
   ],
   vite: {
-    plugins: [
-      tsconfigPaths(),
-    ],
     resolve: {
       alias: {
-        '~types': resolve(__dirname, './src/types'),
-        '~': resolve(__dirname, './src'),
-        '@': resolve(__dirname, './src'),
+        '~types': './src/types',
+        '~': './src',
+        '@': './src',
       },
     },
   },

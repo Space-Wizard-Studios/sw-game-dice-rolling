7938bafc4141ce21b6f93b96c2b53f7637b59a7a
Danilo Nobre <danilonobren@gmail.com>
2024-09-10 17:59:44 -0300

    default paddings and gaps

--- game-prototype/src/components/Dialogue/Dialogue.tsx
+++ game-prototype/src/components/Dialogue/Dialogue.tsx
@@ -30,15 +30,17 @@ export const Dialogue: Component<DialogueProps> = (props) => {
     });
 
     return (
-        <div class={cn('p-2 gap-2 ', props.class)}>
-            {/* <h3>Phase: {gameState.currentState.name}</h3> */}
-            <div
-                ref={messagesContainer}
-                class='flex flex-col h-full overflow-y-auto p-2 gap-2 bg-black bg-opacity-25 border-2 rounded-md border-black border-opacity-50'
-            >
-                {dialogueStore.messages?.map(message => (
-                    <Message message={message} />
-                ))}
+        <div class={cn('gap-2', props.class)}>
+            <div class='flex p-2 h-full w-full bg-neutral-700 bg-opacity-25 rounded-md'>
+                {/* <h3>Phase: {gameState.currentState.name}</h3> */}
+                <div
+                    ref={messagesContainer}
+                    class='flex flex-col h-full w-full overflow-y-auto p-2 gap-2 bg-black bg-opacity-25 border-2 rounded-md border-black border-opacity-50'
+                >
+                    {dialogueStore.messages?.map(message => (
+                        <Message message={message} />
+                    ))}
+                </div>
             </div>
         </div>
     );
--- game-prototype/src/components/Scenes/Gameplay.tsx
+++ game-prototype/src/components/Scenes/Gameplay.tsx
@@ -9,9 +9,9 @@ export const GameplayScene = () => {
     console.log('Rendering GameplayScene...');
 
     return (
-        <section class='h-full gap-2 bg-slate-900 text-white'>
-            <Board class='h-2/3 gap-2 p-2 w-full overflow-hidden' />
-            <Dialogue class='h-1/3 gap-2 p-2 w-full overflow-hidden' />
+        <section class='flex flex-col h-full p-2 gap-2 bg-slate-900 text-white'>
+            <Board class='h-2/3 w-full overflow-hidden' />
+            <Dialogue class='h-1/3 w-full overflow-hidden' />
         </section>
     );
 };

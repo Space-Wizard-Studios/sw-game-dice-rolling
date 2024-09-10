fb6525b163c5a15b7d9a403775efadbb967fba88
Danilo Nobre <danilonobren@gmail.com>
2024-09-10 18:05:33 -0300

    how to center a div?

--- game-prototype/src/components/Scenes/MainMenu.tsx
+++ game-prototype/src/components/Scenes/MainMenu.tsx
@@ -5,7 +5,7 @@ export const MainMenuScene = () => {
     console.log('Rendering MainMenuScene...');
 
     return (
-        <section class='h-full w-full'>
+        <section class='flex h-full w-full items-center justify-center'>
             <button onClick={startGameplay}>Start</button>
         </section>
     );

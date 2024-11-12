a99f3f8256ab205ed4e9be706f3961da5040e19f
Danilo Nobre <danilonobren@gmail.com>
2024-11-12 13:20:35 -0300

    fixing actions display on dice info

--- game/components/DiceGeneator.cs
+++ game/components/DiceGeneator.cs
@@ -1,6 +1,6 @@
 using Godot;
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 public partial class DiceGeneator : Node {
 	[Export]
@@ -11,46 +11,54 @@ public partial class DiceGeneator : Node {
 
 	private Dice<DiceSide> _dice;
 
+	private const int MinSides = 4;
+	private const int MaxSides = 100;
+	private static readonly int[] ValidSides = { 4, 6, 8, 10, 20, 100 };
+
 	public override void _Ready() {
-		if (DiceActionsResource == null) {
-			GD.PrintErr("DiceActionsResource is not assigned");
-			return;
-		}
+		if (!ValidateResources()) return;
 
-		if (DiceIconResource == null) {
-			GD.PrintErr("DiceIconResource is not assigned");
-			return;
-		}
-
-		// Generate a random dice
 		_dice = CreateRandomDice();
 		GD.Print($"Generated Dice: {_dice.Name}");
 		GD.Print($"Number of Sides: {_dice.Sides}");
 
-		// Update DiceDisplay
 		UpdateDiceDisplay();
+
+		var updateButton = GetNode<Button>("UpdateButton");
+		if (updateButton != null) {
+			updateButton.Connect("pressed", new Callable(this, nameof(OnUpdateButtonPressed)));
+		}
+		else {
+			GD.PrintErr("UpdateButton node not found");
+		}
+	}
+
+	private bool ValidateResources() {
+		if (DiceActionsResource == null) {
+			GD.PrintErr("DiceActionsResource is not assigned");
+			return false;
+		}
+
+		if (DiceIconResource == null) {
+			GD.PrintErr("DiceIconResource is not assigned");
+			return false;
+		}
+
+		return true;
 	}
 
 	private Dice<DiceSide> CreateRandomDice() {
 		var random = new Random();
-		int sides = random.Next(1, 7); // Randomly select between 4, 6, 8, 10, 20, 100 sides
-		sides = sides switch {
-			1 => 4,
-			2 => 6,
-			3 => 8,
-			4 => 10,
-			5 => 20,
-			6 => 100,
-			_ => 6
-		};
+		int sides = ValidSides[random.Next(ValidSides.Length)];
 
-		var actions = new Godot.Collections.Array<DiceSide>();
-		for (int i = 0; i < sides; i++) {
-			int randomIndex = random.Next(DiceActionsResource.Actions.Count);
-			var action = DiceActionsResource.Actions[randomIndex];
-			GD.Print($"Selected Action: {action.Name} - {action.Description}");
-			actions.Add(new DiceSide(action.Name, action.Abbreviation, action.Description, action.BackgroundColor, action.TextColor));
-		}
+		var actions = new Godot.Collections.Array<DiceSide>(
+			Enumerable.Range(0, sides).Select(_ => {
+				int randomIndex = random.Next(DiceActionsResource.Actions.Count);
+				var action = DiceActionsResource.Actions[randomIndex];
+				GD.Print($"Selected Action: {action.Name} - {action.Description}");
+				return new DiceSide(action.Name, action.Abbreviation, action.Description, action.BackgroundColor, action.TextColor);
+			}).ToArray()
+		);
 
 		return new Dice<DiceSide>(Guid.NewGuid().ToString(), $"D{sides}", actions, DiceLocation.None);
 	}
@@ -65,29 +73,59 @@ public partial class DiceGeneator : Node {
 
 		var actionsListContainer = diceDisplay.GetNode<VBoxContainer>("DiceInfo/ActionsInfo/ActionsList");
 
-		// Clear all children from the actions list container
-		foreach (Node child in actionsListContainer.GetChildren()) {
-			actionsListContainer.RemoveChild(child);
-			child.QueueFree();
+		// Hide the ActionItemTemplate
+		var actionItemTemplate = actionsListContainer.GetNode<Control>("ActionItemTemplate");
+		if (actionItemTemplate != null) {
+			actionItemTemplate.Visible = false;
 		}
 
-		for (int i = 0; i < _dice.Sides; i++) {
-			var action = _dice.Actions[i];
+		// Clear existing children except the template
+		foreach (Node child in actionsListContainer.GetChildren()) {
+			if (child != actionItemTemplate) {
+				child.QueueFree();
+			}
+		}
 
-			var actionItem = new Control();
-			var actionColor = new ColorRect {
-				Color = action.BackgroundColor,
-				CustomMinimumSize = new Vector2(0, 25)
-			};
-			actionItem.AddChild(actionColor);
-
-			var actionLabel = new Label {
-				Text = $"Side {i + 1}: {action.Name} - {action.Description}",
-				Modulate = action.TextColor
-			};
-			actionColor.AddChild(actionLabel);
-
+		foreach (var action in _dice.Actions.Select((value, index) => new { value, index })) {
+			var actionItem = CreateActionItem(action.value, action.index + 1, actionItemTemplate);
 			actionsListContainer.AddChild(actionItem);
 		}
+	}
+
+	private Control CreateActionItem(DiceSide action, int sideNumber, Control actionItemTemplate) {
+		if (actionItemTemplate == null) {
+			GD.PrintErr("ActionItemTemplate node not found");
+			return null;
+		}
+
+		var actionItem = actionItemTemplate.Duplicate() as Control;
+		if (actionItem == null) {
+			GD.PrintErr("Failed to duplicate ActionItemTemplate");
+			return null;
+		}
+
+		actionItem.Visible = true;
+
+		var actionColor = actionItem.GetNode<ColorRect>("ActionColor");
+		if (actionColor == null) {
+			GD.PrintErr("ActionColor node not found in ActionItemTemplate");
+			return null;
+		}
+		actionColor.Color = action.BackgroundColor;
+
+		var actionLabel = actionItem.GetNode<Label>("ActionLabel");
+		if (actionLabel == null) {
+			GD.PrintErr("ActionLabel node not found in ActionItemTemplate");
+			return null;
+		}
+		actionLabel.Text = $"Side {sideNumber}: {action.Name} - {action.Description}";
+		actionLabel.Modulate = action.TextColor;
+
+		return actionItem;
+	}
+
+	private void OnUpdateButtonPressed() {
+		_dice = CreateRandomDice();
+		UpdateDiceDisplay();
 	}
 }
---
+++ game/components/DiceInspector.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class DiceInspector : Control {
+	[Export]
+	private NodePath diceInfoPath;
+	[Export]
+	private NodePath diceNamePath;
+	[Export]
+	private NodePath interactiveNodePath;
+
+	private Control diceInfo;
+	private Label diceName;
+	private Control interactiveNode;
+	private bool isVisible = false;
+
+	public override void _Ready() {
+		diceInfo = GetNode<Control>(diceInfoPath);
+		diceName = GetNode<Label>(diceNamePath);
+		interactiveNode = GetNode<Control>(interactiveNodePath);
+
+		if (diceInfo == null || diceName == null || interactiveNode == null) {
+			GD.PrintErr("DiceInfo, DiceName, or HoverNode not found. Check the paths.");
+			return;
+		}
+
+		diceInfo.Visible = false;
+		diceName.Visible = false;
+
+		interactiveNode.Connect("gui_input", new Callable(this, nameof(OnMouseClick)));
+	}
+
+	private void OnMouseClick(InputEvent @event) {
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed) {
+			isVisible = !isVisible;
+			diceInfo.Visible = isVisible;
+			diceName.Visible = isVisible;
+		}
+	}
+}

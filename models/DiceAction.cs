e3f956d16ea7457592d97a8614e195fa42d55b23
Danilo Nobre <danilonobren@gmail.com>
2024-11-14 19:58:35 -0300

    trying to make the quantity number field visible only when Quantity is seted to Number

--- models/DiceAction.cs
+++ models/DiceAction.cs
@@ -1,45 +1,74 @@
 using Godot;
-using System.Collections.Generic;
 
 public enum TargetCategory {
 	Enemy,
 	Ally,
 	Self,
 	Any,
-	Nothing
+	Nothing,
+}
+
+public enum QuantityCategory {
+	None,
+	All,
+	Number
 }
 
 [Tool]
 public partial class DiceAction : Resource {
 	[Export]
 	public string Name { get; set; }
+
 	[Export]
 	public string Description { get; set; }
-	[Export]
-	public Godot.Collections.Array<DiceMana> RequiredMana { get; set; } = new Godot.Collections.Array<DiceMana>();
 
+	[Export]
+	public Godot.Collections.Array<DiceMana> RequiredMana { get; set; } =
+		new Godot.Collections.Array<DiceMana>();
+
+	[ExportCategory("Target Options")]
 	[Export(PropertyHint.Enum, "Enemy,Ally,Self,Any,Nothing")]
 	public TargetCategory Category { get; set; }
 
-	private int targetQuantity;
-	[Export(PropertyHint.Range, "-1,100,1")]
-	public int TargetQuantity {
-		get => targetQuantity;
+	private QuantityCategory _quantity = QuantityCategory.None;
+	[Export(PropertyHint.Enum, "None,All,Number")]
+	public QuantityCategory Quantity {
+		get => _quantity;
 		set {
-			if (value >= -1) {
-				targetQuantity = value;
-			}
-			else {
-				GD.PrintErr("Invalid TargetQuantity value. Use -1 for all, 0 for nothing, or a positive number.");
+			if (_quantity != value) {
+				_quantity = value;
+				NotifyPropertyListChanged();
 			}
 		}
 	}
 
+	public int NumberQuantity { get; set; } = 1;
+
 	public DiceAction() { }
 
-	public DiceAction(string name, string description, Godot.Collections.Array<DiceMana> requiredMana) {
+	public DiceAction(
+		string name,
+		string description,
+		Godot.Collections.Array<DiceMana> requiredMana
+	) {
 		Name = name;
 		Description = description;
 		RequiredMana = requiredMana;
+	}
+
+	public override Godot.Collections.Array<Godot.Collections.Dictionary> _GetPropertyList() {
+		var propertyList = base._GetPropertyList();
+		bool isQuantityVisible = Quantity == QuantityCategory.Number;
+
+		if (isQuantityVisible) {
+			var numberQuantityProperty = new Godot.Collections.Dictionary {
+				{ "name", "NumberQuantity" },
+				{ "type", (int)Variant.Type.Int },
+				{ "usage", (int)PropertyUsageFlags.Default }
+			};
+			propertyList.Add(numberQuantityProperty);
+		}
+
+		return propertyList;
 	}
 }

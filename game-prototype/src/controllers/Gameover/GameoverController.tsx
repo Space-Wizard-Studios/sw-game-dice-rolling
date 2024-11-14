18fc9ff4376936b93bcb7bfec49ccd48c5e32646
Danilo Nobre <danilonobren@gmail.com>
2024-11-14 11:51:21 -0300

    moving prototype to another repo

--- game-prototype/src/controllers/Gameover/GameoverController.tsx
+++
@@ -1,8 +0,0 @@
-import { updateGameScene } from '@helpers/updateGameState';
-import type { GameSceneType } from '@models/scenes/Scenes';
-
-const scene: GameSceneType = 'gameoverScene';
-
-export async function startGameover() {
-	updateGameScene(scene, 'gameoverPlaceholder');
-}
--- game-prototype/src/controllers/MainMenu/MainMenuController.tsx
+++
@@ -1,8 +0,0 @@
-import { updateGameScene } from '@helpers/updateGameState';
-import type { GameSceneType } from '@models/scenes/Scenes';
-
-const scene: GameSceneType = 'mainMenuScene';
-
-export async function startMainMenu() {
-	updateGameScene(scene, 'mainMenuPlaceholder');
-}

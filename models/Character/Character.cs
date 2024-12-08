b8661da3268f93a5e84f80a9d23cf25e2130039f
Danilo Nobre <danilonobren@gmail.com>
2024-12-08 17:04:44 -0300

    changing indentation to spaces

--- models/Character/Character.cs
+++ models/Character/Character.cs
@@ -6,50 +6,50 @@ namespace DiceRoll.Models;
 [GlobalClass]
 public partial class Character : Resource {
 
-	[Export]
-	public string Id { get; set; } = Guid.NewGuid().ToString();
+    [Export]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
 
-	[Export]
-	public string? Name { get; set; }
+    [Export]
+    public string? Name { get; set; }
 
-	[Export]
-	public Role? Role { get; set; }
+    [Export]
+    public Role? Role { get; set; }
 
-	[Export]
-	public int DiceCapacity { get; set; } = 0;
+    [Export]
+    public int DiceCapacity { get; set; } = 0;
 
-	[Export]
-	public Texture? Portrait { get; set; }
+    [Export]
+    public Texture? Portrait { get; set; }
 
-	[Export]
-	public SpriteFrames? CharacterSprite { get; set; }
+    [Export]
+    public SpriteFrames? CharacterSprite { get; set; }
 
-	private float _spritePositionX;
-	[Export]
-	public float SpritePositionX {
-		get => _spritePositionX;
-		set {
-			_spritePositionX = value;
-			EmitChanged();
-		}
-	}
+    private float _spritePositionX;
+    [Export]
+    public float SpritePositionX {
+        get => _spritePositionX;
+        set {
+            _spritePositionX = value;
+            EmitChanged();
+        }
+    }
 
-	private float _spritePositionY;
-	[Export]
-	public float SpritePositionY {
-		get => _spritePositionY;
-		set {
-			_spritePositionY = value;
-			EmitChanged();
-		}
-	}
+    private float _spritePositionY;
+    [Export]
+    public float SpritePositionY {
+        get => _spritePositionY;
+        set {
+            _spritePositionY = value;
+            EmitChanged();
+        }
+    }
 
-	[Export]
-	public bool ShowShadow { get; set; }
+    [Export]
+    public bool ShowShadow { get; set; }
 
-	[Export]
-	public SpriteFrames? ShadowSprite { get; set; }
+    [Export]
+    public SpriteFrames? ShadowSprite { get; set; }
 
-	public Character() {
-	}
+    public Character() {
+    }
 }

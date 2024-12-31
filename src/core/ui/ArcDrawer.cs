84bebadaf0bdc76bb138182d5033faf57526a418
Danilo Nobre <danilonobren@gmail.com>
2024-12-31 00:47:17 -0300

    feat: add CharacterLocation model and update project settings for aspect ratio handling; remove unused assets and optimize texture imports

--- src/core/ui/ArcDrawer.cs
+++ src/core/ui/ArcDrawer.cs
@@ -17,12 +17,12 @@ public partial class ArcDrawer : Node2D {
         AddChild(_arc);
     }
 
-    public override void _Process(double delta) {
-        if (_isDrawing && _selectedCharacter is not null) {
-            Vector2 mousePosition = GetGlobalMousePosition();
-            DrawArc(_selectedCharacter.GlobalPosition, mousePosition);
-        }
-    }
+    // public override void _Process(double delta) {
+    //     if (_isDrawing && _selectedCharacter is not null) {
+    //         Vector2 mousePosition = GetGlobalMousePosition();
+    //         DrawArc(_selectedCharacter.GlobalPosition, mousePosition);
+    //     }
+    // }
 
     public void SetSelectedCharacter(CharacterComponent character) {
         _selectedCharacter = character;
@@ -31,14 +31,14 @@ public partial class ArcDrawer : Node2D {
         QueueRedraw();
     }
 
-    public void SetSelectedEnemy(CharacterComponent enemy) {
-        _selectedEnemy = enemy;
-        _isDrawing = false;
-        if (_selectedCharacter is not null) {
-            DrawArc(_selectedCharacter.GlobalPosition, _selectedEnemy.GlobalPosition);
-        }
-        QueueRedraw();
-    }
+    // public void SetSelectedEnemy(CharacterComponent enemy) {
+    //     _selectedEnemy = enemy;
+    //     _isDrawing = false;
+    //     if (_selectedCharacter is not null) {
+    //         DrawArc(_selectedCharacter.GlobalPosition, _selectedEnemy.GlobalPosition);
+    //     }
+    //     QueueRedraw();
+    // }
 
     private void DrawArc(Vector2 start, Vector2 end) {
         if (_arc is not null) {
--- src/models/CharacterActions/Resources/EffectTypes/DamageEffect/DamageEffect.cs
+++ src/models/CharacterActions/Resources/EffectTypes/DamageEffect/DamageEffect.cs
@@ -2,8 +2,17 @@ using System.Linq;
 
 namespace DiceRoll.Models.CharacterActions.Effects;
 
-public partial class DamageEffect(int damage) : Effect {
-    private readonly int _damage = damage;
+public partial class DamageEffect : Effect {
+    private readonly int _damage;
+
+    public DamageEffect() {
+    }
+
+    public DamageEffect(TargetType targetType, QuantityType quantityType, int numberQuantity) {
+        TargetType = targetType;
+        QuantityType = quantityType;
+        NumberQuantity = numberQuantity;
+    }
 
     public override void Apply(IActionContext context) {
         var healthAttribute = context.Target.Attributes.FirstOrDefault(attr => attr.Type?.Name == "Health");
@@ -13,4 +22,5 @@ public partial class DamageEffect(int damage) : Effect {
             context.Target.EmitSignal(nameof(Character.AttributeChanged), context.Target, healthAttribute.Type!);
         }
     }
+
 }

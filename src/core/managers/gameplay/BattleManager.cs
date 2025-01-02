8635118c5d1a3523c6ec50e1857b630db0513c21
Danilo Nobre <danilonobren@gmail.com>
2025-01-02 18:46:40 -0300

    refactor: comment out unused properties and methods in BattleManager

--- src/core/managers/gameplay/BattleManager.cs
+++ src/core/managers/gameplay/BattleManager.cs
@@ -7,11 +7,11 @@ using DiceRoll.Stores;
 namespace DiceRoll.Managers;
 
 public partial class BattleManager : Node3D {
-    [Export]
-    public PackedScene? GameLogScene { get; set; }
+    // [Export]
+    // public PackedScene? GameLogScene { get; set; }
 
-    [Export]
-    public DiceManaResources? DiceManaResources { get; set; }
+    // [Export]
+    // public DiceManaResources? DiceManaResources { get; set; }
 
     public override void _Ready() {
         GD.Print("Battle _Ready called");
@@ -23,7 +23,7 @@ public partial class BattleManager : Node3D {
         RunGameLogTests();
 
         // Run dice tests
-        RunDiceTests();
+        // RunDiceTests();
     }
 
     private static void RunCharacterTests() {
@@ -66,26 +66,26 @@ public partial class BattleManager : Node3D {
         GameLogStore.Instance.AddGameLogLine(newLine);
     }
 
-    private void RunDiceTests() {
-        if (DiceManaResources is null) {
-            GD.PrintErr("DiceManaResources is not assigned");
-            return;
-        }
+    // private void RunDiceTests() {
+    //     if (DiceManaResources is null) {
+    //         GD.PrintErr("DiceManaResources is not assigned");
+    //         return;
+    //     }
 
-        var dice = DiceFactory.CreateD20(DiceManaResources);
-        GD.Print($"Generated Dice: {dice.Name} with {dice.Sides} sides");
+    //     var dice = DiceFactory.CreateD20(DiceManaResources);
+    //     GD.Print($"Generated Dice: {dice.Name} with {dice.Sides} sides");
 
-        var diceTimestamp = DateTime.Now.ToString("HH:mm");
+    //     var diceTimestamp = DateTime.Now.ToString("HH:mm");
 
-        // Add a message to the log system with the dice sides as lines
-        var diceLogLines = new List<GameLogLine>();
-        for (int i = 0; i < dice.Sides; i++) {
-            var mana = dice.Manas[i];
-            var line = new GameLogLine(GameLogLineType.Default, $"[url={mana.Name}]Side {i + 1}: {mana.Name}[/url]");
-            diceLogLines.Add(line);
-        }
+    //     // Add a message to the log system with the dice sides as lines
+    //     var diceLogLines = new List<GameLogLine>();
+    //     for (int i = 0; i < dice.Sides; i++) {
+    //         var mana = dice.Manas[i];
+    //         var line = new GameLogLine(GameLogLineType.Default, $"[url={mana.Name}]Side {i + 1}: {mana.Name}[/url]");
+    //         diceLogLines.Add(line);
+    //     }
 
-        var diceLogMessage = new GameLogMessage(dice.Name, diceTimestamp, diceLogLines);
-        GameLogStore.Instance.AddGameLogMessage(diceLogMessage);
-    }
+    //     var diceLogMessage = new GameLogMessage(dice.Name, diceTimestamp, diceLogLines);
+    //     GameLogStore.Instance.AddGameLogMessage(diceLogMessage);
+    // }
 }

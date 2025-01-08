24dc2ce365afeec602cda4636055b62a57bf5454
Danilo Nobre <danilonobren@gmail.com>
2025-01-07 23:08:15 -0300

    feat: enhance LineRenderer with improved mesh generation and corner handling

--- src/addons/LineRenderer/LineRenderer.cs
+++ src/addons/LineRenderer/LineRenderer.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 [Tool]
 public partial class LineRenderer : MeshInstance3D {
-    [Export] public Godot.Collections.Array<Vector3> points = [new Vector3(0, 0, 0), new Vector3(0, 5, 0)];
+    [Export] public Godot.Collections.Array<Vector3> points = new Godot.Collections.Array<Vector3> { new Vector3(0, 0, 0), new Vector3(0, 5, 0) };
     [Export] public float startThickness = 0.1f;
     [Export] public float endThickness = 0.1f;
     [Export] public int cornerSmooth = 5;
@@ -37,50 +37,51 @@ public partial class LineRenderer : MeshInstance3D {
     }
 
     private void GenerateMesh() {
-    float progressStep = 1.0f / points.Count;
-    float progress = 0;
-    float thickness = Mathf.Lerp(startThickness, endThickness, progress);
-    float nextThickness = Mathf.Lerp(startThickness, endThickness, progress + progressStep);
+        float progressStep = 1.0f / points.Count;
+        float progress = 0;
+        float thickness = Mathf.Lerp(startThickness, endThickness, progress);
+        float nextThickness = Mathf.Lerp(startThickness, endThickness, progress + progressStep);
 
-    immediateMesh?.ClearSurfaces();
-    immediateMesh?.SurfaceBegin(Mesh.PrimitiveType.Triangles);
+        immediateMesh?.ClearSurfaces();
+        immediateMesh?.SurfaceBegin(Mesh.PrimitiveType.Triangles);
 
-    for (int i = 0; i < points.Count - 1; i++) {
-        Vector3 startPoint = points[i];
-        Vector3 endPoint = points[i + 1];
+        for (int i = 0; i < points.Count - 1; i++) {
+            Vector3 startPoint = points[i];
+            Vector3 endPoint = points[i + 1];
 
-        if (globalCoords) {
-            startPoint = ToLocal(startPoint);
-            endPoint = ToLocal(endPoint);
+            if (globalCoords) {
+                startPoint = ToLocal(startPoint);
+                endPoint = ToLocal(endPoint);
+            }
+
+            Vector3 startOrthogonal = CalculateOrthogonal(startPoint, endPoint, thickness);
+            Vector3 endOrthogonal = CalculateOrthogonal(startPoint, endPoint, nextThickness);
+
+            Vector3 startPointOuter = startPoint + startOrthogonal;
+            Vector3 startPointInner = startPoint - startOrthogonal;
+            Vector3 endPointOuter = endPoint + endOrthogonal;
+            Vector3 endPointInner = endPoint - endOrthogonal;
+
+            if (i == 0 && drawCaps) {
+                Cap(startPoint, endPoint, thickness, capSmooth);
+            }
+
+            AddVertices(startPointOuter, startPointInner, endPointOuter, endPointInner);
+
+            if (i == points.Count - 2 && drawCaps) {
+                Cap(endPoint, startPoint, nextThickness, capSmooth);
+            }
+            else if (drawCorners && i < points.Count - 2) {
+                DrawCorner(i, endPoint, endOrthogonal, nextThickness);
+            }
+
+            progress += progressStep;
+            thickness = Mathf.Lerp(startThickness, endThickness, progress);
+            nextThickness = Mathf.Lerp(startThickness, endThickness, progress + progressStep);
         }
 
-        Vector3 startOrthogonal = CalculateOrthogonal(startPoint, endPoint, thickness);
-        Vector3 endOrthogonal = CalculateOrthogonal(startPoint, endPoint, nextThickness);
-
-        Vector3 startPointOuter = startPoint + startOrthogonal;
-        Vector3 startPointInner = startPoint - startOrthogonal;
-        Vector3 endPointOuter = endPoint + endOrthogonal;
-        Vector3 endPointInner = endPoint - endOrthogonal;
-
-        if (i == 0 && drawCaps) {
-            Cap(startPoint, endPoint, thickness, capSmooth);
-        }
-
-        AddVertices(startPointOuter, startPointInner, endPointOuter, endPointInner);
-
-        if (i == points.Count - 2 && drawCaps) {
-            Cap(endPoint, startPoint, nextThickness, capSmooth);
-        } else if (drawCorners && i < points.Count - 2) {
-            DrawCorner(i, endPoint, endOrthogonal, nextThickness);
-        }
-
-        progress += progressStep;
-        thickness = Mathf.Lerp(startThickness, endThickness, progress);
-        nextThickness = Mathf.Lerp(startThickness, endThickness, progress + progressStep);
+        immediateMesh?.SurfaceEnd();
     }
-
-    immediateMesh?.SurfaceEnd();
-}
 
     private Vector3 CalculateOrthogonal(Vector3 A, Vector3 B, float thickness) {
         Vector3 AB = B - A;
@@ -123,6 +124,8 @@ public partial class LineRenderer : MeshInstance3D {
     }
 
     private void DrawCorner(int i, Vector3 B, Vector3 orthogonalABEnd, float nextThickness) {
+        if (i + 2 >= points.Count) return;
+
         Vector3 C = points[i + 2];
         if (globalCoords) {
             C = ToLocal(C);
@@ -138,6 +141,20 @@ public partial class LineRenderer : MeshInstance3D {
         }
         else {
             Corner(B, B - orthogonalBCStart, B - orthogonalABEnd, cornerSmooth);
+        }
+    }
+
+    private void Corner(Vector3 center, Vector3 start, Vector3 next, int smoothing) {
+        Vector3 axis = (start - center).Normalized();
+        float angleStep = Mathf.Pi / smoothing;
+
+        for (int i = 0; i <= smoothing; i++) {
+            float angle = angleStep * i;
+            Vector3 offset = axis.Rotated(Vector3.Up, angle) * (start - center).Length();
+            Vector3 vertex = center + offset;
+
+            immediateMesh?.SurfaceAddVertex(vertex);
+            immediateMesh?.SurfaceSetUV(new Vector2(i / (float)smoothing, 1));
         }
     }
 
@@ -165,17 +182,4 @@ public partial class LineRenderer : MeshInstance3D {
         }
     }
 
-    private void Corner(Vector3 center, Vector3 start, Vector3 end, int smoothing) {
-        Vector3 axis = (start - center).Normalized();
-        float angleStep = Mathf.Pi / smoothing;
-
-        for (int i = 0; i <= smoothing; i++) {
-            float angle = angleStep * i;
-            Vector3 offset = axis.Rotated(axis.Cross(end - center).Normalized(), angle) * (start - center).Length();
-            Vector3 vertex = center + offset;
-
-            immediateMesh?.SurfaceAddVertex(vertex);
-            immediateMesh?.SurfaceSetUV(new Vector2(i / (float)smoothing, 1));
-        }
-    }
 }

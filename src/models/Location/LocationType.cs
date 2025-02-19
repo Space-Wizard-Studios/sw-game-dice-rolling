0b8d8a1b2f0382c3605518bd696293b77800d948
Danilo Nobre <danilonobren@gmail.com>
2025-02-19 15:00:09 -0300

    feat: add ILocation interface and implement it in LocationType model

--- src/models/Location/LocationType.cs
+++ src/models/Location/LocationType.cs
@@ -1,10 +1,11 @@
 using Godot;
+using DiceRolling.Interfaces.Location;
 
 namespace DiceRolling.Models.Locations;
 
 [Tool]
 [GlobalClass]
-public partial class LocationType : Resource {
+public partial class LocationType : Resource, ILocation {
     [Export] public string Name { get; set; } = "";
     [Export] public string Description { get; set; } = "";
     [Export] public int TotalSlots { get; set; } = 6;

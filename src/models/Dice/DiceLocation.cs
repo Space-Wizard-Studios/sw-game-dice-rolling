d7adbc9468144b0f54abd8eb55842b634e1fce89
Danilo Nobre <danilonobren@gmail.com>
2025-02-19 17:41:26 -0300

    refactor: update DiceLocation class to use constructor for property initialization

--- src/models/Dice/DiceLocation.cs
+++ src/models/Dice/DiceLocation.cs
@@ -3,7 +3,12 @@ using DiceRolling.Interfaces.Dice;
 
 namespace DiceRolling.Models.Dice;
 
-public partial class DiceLocation(DiceLocationCategory locationCategory, string? characterId = null) : Resource, IDiceLocation {
-    [Export] public DiceLocationCategory LocationCategory { get; set; } = locationCategory;
-    [Export] public string? CharacterId { get; set; } = characterId;
+public partial class DiceLocation : Resource, IDiceLocation {
+    [Export] public DiceLocationCategory LocationCategory { get; set; }
+    [Export] public string? CharacterId { get; set; }
+
+    public DiceLocation(DiceLocationCategory locationCategory, string? characterId = null) {
+        LocationCategory = locationCategory;
+        CharacterId = characterId;
+    }
 }

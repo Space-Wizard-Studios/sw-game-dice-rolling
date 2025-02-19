c2d1c48d6aebdf371e610a02eb23bdd2880ede23
Danilo Nobre <danilonobren@gmail.com>
2025-02-19 15:41:28 -0300

    feat: add IEffect interface and implement it in EffectType model with documentation, closes #92

--- src/models/Effects/EffectType.cs
+++ src/models/Effects/EffectType.cs
@@ -1,8 +1,10 @@
 using Godot;
 using DiceRolling.Interfaces.Action;
+using DiceRolling.Interfaces.Effects;
+
 namespace DiceRolling.Models.Actions.Effects;
 
 [Tool]
-public abstract partial class EffectType : Resource {
+public abstract partial class EffectType : Resource, IEffect {
     public abstract void Apply(IActionContext context);
 }

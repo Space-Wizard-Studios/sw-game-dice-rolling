2aa8af3c2ab481e6f288ef0cf4e020f57eeb7882
Danilo Nobre <danilonobren@gmail.com>
2025-02-19 22:17:34 -0300

    refactor: remove unused Dice-related tests

--- src/scenes/playground/DiceDisplay/DisplayActionItem.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-
-public partial class DisplayActionItem : Control {
-	[Export] public ColorRect ActionItemBackground { get; private set; }
-	[Export] public Label ActionItemLabel { get; private set; }
-
-	public override void _Ready() { }
-}

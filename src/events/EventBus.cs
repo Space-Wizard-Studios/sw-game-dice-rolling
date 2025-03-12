609e3645276d4cefa4ef3684cdc65b18cf06764b
Danilo Nobre <danilonobren@gmail.com>
2025-03-11 23:02:49 -0300

    Refactor EventBus singleton implementation for improved instance management

--- src/events/EventBus.cs
+++ src/events/EventBus.cs
@@ -6,26 +6,42 @@ namespace DiceRolling.Events;
 
 [Tool]
 public partial class EventBus : Node {
+    private static EventBus? _instance;
+
     [Signal] public delegate void ComponentSelectedEventHandler(Node component);
     [Signal] public delegate void ComponentUnselectedEventHandler(Node component);
 
-    private static EventBus? _instance;
-
     public static EventBus Instance {
         get {
-            _instance ??= new EventBus();
+            if (_instance == null) {
+                // Tenta obter do Autoload primeiro (quando em cena)
+                if (Engine.GetMainLoop() is SceneTree tree) {
+                    _instance = tree.Root.GetNodeOrNull<EventBus>("/root/EventBus");
+                }
+
+                // Se não encontrar na árvore, cria uma nova instância
+                _instance ??= new EventBus();
+            }
             return _instance;
         }
     }
 
     public override void _Ready() {
-        _instance = this;
+        // Limpa a instância estática se for diferente desta
+        if (_instance != null && _instance != this) {
+            _instance = this;
+        }
+        else {
+            _instance = this;
+        }
     }
 
     public void OnComponentSelected(Node component) {
         EmitSignal(nameof(ComponentSelected), component);
 
-        if (component is SelectableComponent selectableComponent && selectableComponent.GetParent<Entity3D>() is Entity3D parentEntity && parentEntity.Data is { } data) {
+        if (component is SelectableComponent selectableComponent &&
+            selectableComponent.GetParent<Entity3D>() is Entity3D parentEntity &&
+            parentEntity.Data is { } data) {
             GD.Print("Selected Data ID:", data.Id);
         }
     }

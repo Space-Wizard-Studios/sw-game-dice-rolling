92f296fad7dac99b7a1ca4fd905784dfc883cd39
Danilo Nobre <danilonobren@gmail.com>
2025-03-25 11:55:11 -0300

    restarting this shit over

--- src/controllers/battle/BattleState.cs
+++ src/controllers/battle/BattleState.cs
@@ -1,9 +1,8 @@
 namespace DiceRolling.Controllers;
 
+/// <summary>
+/// Define os possíveis estados da batalha.
+/// </summary>
 public enum BattleState {
-    Preparation,    // Preparação inicial da batalha
-    ActionDeclaration,  // Jogadores e inimigos declarando ações
-    TurnExecution,  // Executando turnos com base na iniciativa
-    BattleEnd,      // Batalha finalizada (vitória ou derrota)
-    Paused          // Batalha pausada temporariamente
+
 }
